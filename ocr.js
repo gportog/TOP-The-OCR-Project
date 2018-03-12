f6edcdc5c3a199d0979b4a2ea0c6c078095c9900
Gustavo <git@ibm.com>
2018-03-12 15:19:42 -0300

    Add handlers to the ocr conversion route :construction:

--- ocr.js
+++ ocr.js
@@ -9,10 +9,17 @@ var options = {
 exports.tesseract_handler = (img_path, cb) => {
     tesseract.process(__dirname + '/' + img_path, options, function(err, text) {
       if(err) {
-        cb(500);
+        let error = {
+          code: 500,
+          message:err
+        }
+        cb(error);
       } else {
-          console.log(text);
-        cb(text);
+        let data = {
+          code: 200,
+          message: text
+        }
+        cb(data);
       } 
     });
 };
